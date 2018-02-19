8222473c9212255b6c1969f3826d7f1406b3dabb
bee189 <34267418+bee189@users.noreply.github.com>
2018-02-19 04:03:58 +0100

    Update quorum.h

--- a2-synchrotron-contiki/apps/chaos/quorum-copy/quorum.h
+++ a2-synchrotron-contiki/apps/chaos/quorum-copy/quorum.h
@@ -7,13 +7,24 @@
 
 
 enum {
-    WRITE_PROPOSE = 0
+    CAN_WRITE = 0 ,WRITE_PROPOSE = 1, WRITE_DONE =2 ,CAN_READ =3 , READ_PROPOSE = 4, CAN_WRITE_QUERY = 5
 };
 #define PHASE_TO_STR(phase) (\
-   (phase == WRITE_PROPOSE) ? ("WRITE_PROPOSE" ) : \
+   (phase == CAN_WRITE) ? ("CAN_WRITE" ) : \
+		(phase == CAN_READ) ? ("CAN_READ" ) : \
+		   (phase == CAN_WRITE_QUERY) ? ("CAN_WRITE_QUERY" ) : \
+				(phase == WRITE_PROPOSE) ? ("WRITE_PROPOSE" ) : \
+					(phase == WRITE_DONE) ? ("WRITE_DONE" ) : \
+						(phase == READ_PROPOSE) ? ("READ_PROPOSE" ) : \
  )
 
-
+enum {
+    WRITE_HEADER = 0, READ_HEADER = 1, WRITE_QUERY_HEADER = 2
+};
+#define HEADER_TO_STR(msg_type) (\
+   (msg_type == READ_HEADER) ? ("READ_HEADER" ) : \
+		(msg_type == WRITE_HEADER) ? ("WRITE_HEADER" ) : \   
+ )
 
 
 
@@ -26,7 +37,7 @@ enum {
 
 #define MAX_SLOT_LEN_DCO      (MAX_SLOT_LEN*CLOCK_PHI)    //TODO needs calibration
 
-int quorum_round_begin(const uint16_t round_count, const uint8_t app_id, uint16_t* value, uint8_t** final_flags, uint8_t* phase ,uint16_t* time_stamp );
+int quorum_round_begin(const uint16_t round_count, const uint8_t app_id, uint16_t* value, uint8_t** final_flags, uint8_t* phase ,uint16_t* time_stamp, uint8_t* header );
 
 int quorum_is_pending(const uint16_t round_count);
 
